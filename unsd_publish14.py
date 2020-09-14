85432402c6b0c111272c8e63854e16f0e0785e1f
Luis Gonzalez <gonzalezmorales@gmail.com>
2020-09-14 11:59:33 -0400

    Partical update to all countries - up to goal 9

--- unsd_publish14.py
+++ unsd_publish14.py
@@ -48,7 +48,7 @@ count_country = 0
 
 for this_country in countryArray:
 
-    # if this_country['M49'] != '31':
+    # if this_country['M49'] not in ['8', '484']:
     #     continue
 
     # if this_country['Country_Profile'] != '1':
